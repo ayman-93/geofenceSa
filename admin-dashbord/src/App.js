7c7166c3ad6b8e7f9e75d9f56766680aafbe2c32
FaisalAljazaeri <faisal.hassan93@gmail.com>
2020-04-22 03:07:19 +0300

    Finish Basic Chat

--- admin-dashbord/src/App.js
+++ admin-dashbord/src/App.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 // import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Sidebar from "./components/Sidebar";
+import Chat from "./components/Chat";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./components/Sidebar.css";
 
 function App() {
+    const [active, toggleActive] = useState(false);
+
     return (
-        <div className="App">
-            <Sidebar />
-        </div>
+        <Router>
+            <div className="wrapper d-flex align-items-stretch">
+                <Sidebar active={active} />
+                <div id="content" className="p-4 p-md-5">
+                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                        <div className="container-fluid">
+                            <button
+                                type="button"
+                                id="sidebarCollapse"
+                                className="btn btn-primary"
+                                onClick={() => toggleActive(!active)}
+                            >
+                                <i className="fa fa-bars"></i>
+                                <span className="sr-only">Toggle Menu</span>
+                            </button>
+                        </div>
+                    </nav>
+                    <Switch>
+                        <Route exact path="/chat" component={Chat} />
+                    </Switch>
+                </div>
+            </div>
+        </Router>
     );
 }
 
